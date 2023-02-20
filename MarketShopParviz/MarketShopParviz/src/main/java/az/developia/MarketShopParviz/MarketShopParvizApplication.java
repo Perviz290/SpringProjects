560542714663976cdf78db4b6215db68a20df10d
Perviz290 <112194956+Perviz290@users.noreply.github.com>
2023-02-20 08:53:02 -0800

    Market-Project-bean

---
+++ MarketShopParviz/MarketShopParviz/src/main/java/az/developia/MarketShopParviz/MarketShopParvizApplication.java
@@ -0,0 +1,13 @@
+package az.developia.MarketShopParviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketShopParvizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketShopParvizApplication.class, args);
+	}
+
+}
---
+++ MarketShopParviz/MarketShopParviz/src/test/java/az/developia/MarketShopParviz/MarketShopParvizApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.MarketShopParviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketShopParvizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
